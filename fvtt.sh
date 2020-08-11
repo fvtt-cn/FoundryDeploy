25c99454db90a4851e506090b5c26f67919e3fcf
hmqgg <mitchx7@outlook.com>
2020-08-11 17:24:08 +0800

    Update fvtt.sh

--- fvtt.sh
+++ fvtt.sh
@@ -127,7 +127,7 @@ if test -z "$@" || test "$@" == "recreate"; then
 if [ -x "$(command -v docker)" ]; then
     information "Docker 已安装"
 else
-    warning "Docker 未安装，安装中...（境内服务器可能较慢，耐心等待）"
+    warning "Docker 未安装，安装中..."
     curl -fsSL https://get.docker.com | sh -s -- --mirror Aliyun
     
     # CentOS 安装后启动 Docker 服务
