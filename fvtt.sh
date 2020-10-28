678527af5593f9eec5ccc5d8b5631503d5619223
hmqgg <mitchx7@outlook.com>
2020-10-28 09:39:32 +0800

    Set Minify

--- fvtt.sh
+++ fvtt.sh
@@ -318,7 +318,10 @@ caddyrun="${caddyrun} caddy"
 eval $caddyrun && docker container inspect $caddyname >/dev/null 2>&1 && success "Caddy 容器启动成功" || { error "错误：Caddy 容器启动失败" ; exit 7 ; }
 
 # FVTT，使用 root:root 运行避免文件权限问题
-fvttrun="docker run -d --name=${fvttname} --restart=unless-stopped --network=${bridge} -c=${fvttcpu} -e FOUNDRY_UID='root' -e FOUNDRY_GID='root' -v ${fvttvolume}:/data -v ${fvttapp}:/home/foundry -e FOUNDRY_USERNAME='${username}' -e FOUNDRY_PASSWORD='${password}' "
+fvttrun="docker run -d --name=${fvttname} --restart=unless-stopped --network=${bridge} -c=${fvttcpu} "
+fvttrun="${fvttrun}-e FOUNDRY_UID='root' -e FOUNDRY_GID='root' -e FOUNDRY_MINIFY_STATIC_FILES='true' "
+fvttrun="${fvttrun}-v ${fvttvolume}:/data -v ${fvttapp}:/home/foundry "
+fvttrun="${fvttrun}-e FOUNDRY_USERNAME='${username}' -e FOUNDRY_PASSWORD='${password}' "
 [ -n "$version" ] && { [[ $version == http* ]] && fvttrun="${fvttrun}-e FOUNDRY_RELEASE_URL='${version}' " || fvttrun="${fvttrun}-e FOUNDRY_VERSION='${version}' "; }
 [ -n "$adminpass" ] && fvttrun="${fvttrun}-e FOUNDRY_ADMIN_KEY='${adminpass}' "
 [ -n "$domain" ] && fvttrun="${fvttrun}-e FOUNDRY_HOSTNAME='${domain}' -e FOUNDRY_PROXY_SSL='true' -e FOUNDRY_PROXY_PORT='443' "
