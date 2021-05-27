dc987fe7505646626bed5f254119d39eb7b62506
hmqgg <mitchx7@outlook.com>
2021-05-28 01:08:20 +0800

    Fix GHProxy PATCH

--- patches/PATCH_GHProxy.js
+++ patches/PATCH_GHProxy.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const dest = process.argv.slice[2];
+const dest = process.argv[2];
 const files = [
   `${dest}/files/files.mjs`,
   `${dest}/files/files.js`,
@@ -10,8 +10,9 @@ const files = [
 
 for (let file of files) {
   if (fs.existsSync(file)) {
+    console.log(`Applying GHProxy Patch to ${file}`);
     let fileContent = fs.readFileSync(file, 'utf8');
-    fileContent = fileContent.replaceAll(/(const(.(?<!const))+?await fetch\((.+?)(?=,))/g,
+    fileContent = fileContent.replace(/(const(.(?<!const))+?await fetch\((.+?)(?=,))/g,
       "if(!$3.includes('ghproxy.com/')&&($3.includes('github.com/')||$3.includes('raw.githubusercontent.com/')))$3='https://ghproxy.com/'+$3;$1");
     fs.writeFileSync(file, fileContent);
   }
